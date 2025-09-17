3aaab26e2601edddfb43eab8921b7857abacf16f
TangentYeti <104864803+TangentYeti@users.noreply.github.com>
2025-09-17 02:40:17 +0100

    im dumb

--- play.pokemonshowdown.com/src/battle-tooltips.ts
+++ play.pokemonshowdown.com/src/battle-tooltips.ts
@@ -2133,10 +2133,10 @@ export class BattleTooltips {
 		if (!value.value) return value;
 
 		// Other ability boosts
-		if (pokemon.ability === 'exectutioner' && target.hp * 2 <= target.maxhp) {
+		if (target.hp * 2 <= target.maxhp) {
 			value.abilityModify(2, 'Executioner');
 		}
-		if (pokemon.ability === 'eldrich' && move.category === 'Special') {
+		if (move.category === 'Special') {
 			value.abilityModify(0.5, 'Eldrich');
 		}
 		if (pokemon.status === 'brn' && move.category === 'Special') {
@@ -2192,11 +2192,11 @@ export class BattleTooltips {
 				value.abilityModify(1 + 0.1 * i, "Supreme Overlord");
 			}
 		}
-		for (let i = 1; i <= 5 && i <= pokemon.side.faintCounter; i++) {
-			if (pokemon.volatiles[`fallen${i}`]) {
-				value.abilityModify(0.33, "Body Count");
-			}
-		}
+		// for (let i = 1; i <= 5 && i <= pokemon.side.faintCounter; i++) {
+		// 	if (pokemon.volatiles[`fallen${i}`]) {
+		// 		value.abilityModify(0.33, "Body Count");
+		// 	}
+		// }
 		if (target) {
 			if (["MF", "FM"].includes(pokemon.gender + target.gender)) {
 				value.abilityModify(0.75, "Rivalry");
